281efc7bb873ada3d8977ca50f50a7e1b75c6e4c
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-07-05 19:26:58 +0530

    Create EpidemicService.java

---
+++ backend/service/EpidemicService.java
@@ -0,0 +1,27 @@
+package com.anomalia.backend.service;
+
+import com.anomalia.backend.model.EpidemicEvent;
+import com.anomalia.backend.repository.EpidemicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EpidemicService {
+
+    private final EpidemicRepository repository;
+
+    @Autowired
+    public EpidemicService(EpidemicRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<EpidemicEvent> getAllRecent() {
+        return repository.findAll();
+    }
+
+    public void save(EpidemicEvent event) {
+        repository.save(event);
+    }
+}
