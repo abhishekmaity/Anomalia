68ae36450fa5485e711ec4971a3c79791caaf4ea
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-07-05 19:49:54 +0530

    Create SocialTrendService.java

---
+++ backend/service/SocialTrendService.java
@@ -0,0 +1,27 @@
+package com.anomalia.backend.service;
+
+import com.anomalia.backend.model.SocialTrend;
+import com.anomalia.backend.repository.SocialTrendRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SocialTrendService {
+
+    private final SocialTrendRepository repository;
+
+    @Autowired
+    public SocialTrendService(SocialTrendRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<SocialTrend> getAllRecent() {
+        return repository.findAll();
+    }
+
+    public void save(SocialTrend trend) {
+        repository.save(trend);
+    }
+}
