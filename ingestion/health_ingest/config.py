e89f4f40b9866587631d31d8af4b8d91bbf432c1
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-07-06 23:45:45 +0530

    Create config.py

---
+++ ingestion/health_ingest/config.py
@@ -0,0 +1,9 @@
+WHO_API_URL = "https://ghoapi.azureedge.net/api/WHOSIS_000001"  # Example: Life expectancy
+
+DB_CONFIG = {
+    'dbname': 'anomalia',
+    'user': 'postgres',
+    'password': 'postgres',
+    'host': 'localhost',
+    'port': 5432,
+}
