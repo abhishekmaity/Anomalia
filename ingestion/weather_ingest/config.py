8cbad29546f0cccf28c4d929c8d9317c702fbb82
abhishekmaity <abhishekmaity@hotmail.com>
2025-07-06 19:37:04 +0530

    fix API keys and perform one db entry

--- ingestion/weather_ingest/config.py
+++ ingestion/weather_ingest/config.py
@@ -1,11 +1,11 @@
 # config.py
-API_KEY = "cb34f3563f4454cafde947a4023777d0"  # Added 'anomalia' OpenWeatherMap API 
+API_KEY = "bd5e378503939ddaee76f12ad7a97608" # Added 'anomalia' OpenWeatherMap API 
 CITY = "Kolkata"
 UNITS = "metric"
 DB_CONFIG = {
     'dbname': 'anomalia',
     'user': 'postgres',
-    'password': 'postgres',
+    'password': 'anomalia',
     'host': 'localhost',
     'port': 5432,
 }
