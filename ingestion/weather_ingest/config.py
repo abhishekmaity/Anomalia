ecd9563cd5f126a167259a200446f5c417523100
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-07-06 10:30:07 +0530

    Create config.py

---
+++ ingestion/weather_ingest/config.py
@@ -0,0 +1,11 @@
+# config.py
+API_KEY = "your_openweathermap_api_key"  # Replace this
+CITY = "Kolkata"
+UNITS = "metric"
+DB_CONFIG = {
+    'dbname': 'anomalia',
+    'user': 'postgres',
+    'password': 'postgres',
+    'host': 'localhost',
+    'port': 5432,
+}
