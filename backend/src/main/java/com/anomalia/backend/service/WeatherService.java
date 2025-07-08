426f0b9bc895c620652327d255d3ffb9b05f2281
abhishekmaity <abhishekmaity@hotmail.com>
2025-07-08 23:45:26 +0530

    WeatherService.java updated based on MapStruct

--- backend/src/main/java/com/anomalia/backend/service/WeatherService.java
+++ backend/src/main/java/com/anomalia/backend/service/WeatherService.java
@@ -1,24 +1,29 @@
 package com.anomalia.backend.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-
 import com.anomalia.backend.dto.WeatherAnomalyDTO;
 import com.anomalia.backend.mapper.WeatherMapper;
+import com.anomalia.backend.model.WeatherAnomaly;
 import com.anomalia.backend.repository.WeatherRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherService {
 
+    private final WeatherRepository repository;
+    private final WeatherMapper weatherMapper;
+
     @Autowired
-    private WeatherRepository repository;
+    public WeatherService(WeatherRepository repository, WeatherMapper weatherMapper) {
+        this.repository = repository;
+        this.weatherMapper = weatherMapper;
+    }
 
     public Page<WeatherAnomalyDTO> getRecentAnomalies(int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by("recordedAt").descending());
-        return repository.findAll(pageable).map(WeatherMapper::toDTO);
+        Page<WeatherAnomaly> anomalies = repository.findAll(pageable);
+        return anomalies.map(weatherMapper::toDTO);
     }
 }
