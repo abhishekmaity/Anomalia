59c92320cb8b6a5e4c02770acf43287c809d515f
abhishekmaity <abhishekmaity@hotmail.com>
2025-07-08 23:38:26 +0530

    updated with MapStruct

--- backend/src/main/java/com/anomalia/backend/mapper/WeatherMapper.java
+++ backend/src/main/java/com/anomalia/backend/mapper/WeatherMapper.java
@@ -1,18 +1,12 @@
 package com.anomalia.backend.mapper;
 
+import org.mapstruct.Mapper;
+
 import com.anomalia.backend.dto.WeatherAnomalyDTO;
 import com.anomalia.backend.model.WeatherAnomaly;
 
-public class WeatherMapper {
-
-    public static WeatherAnomalyDTO toDTO(WeatherAnomaly entity) {
-        WeatherAnomalyDTO dto = new WeatherAnomalyDTO();
-        dto.setCity(entity.getCity());
-        dto.setTemperature(entity.getTemperature());
-        dto.setHumidity(entity.getHumidity());
-        dto.setPressure(entity.getPressure());
-        dto.setWindSpeed(entity.getWindSpeed());
-        dto.setRecordedAt(entity.getRecordedAt());
-        return dto;
-    }
+@Mapper(componentModel = "spring")
+public interface WeatherMapper {
+    WeatherAnomalyDTO toDTO(WeatherAnomaly entity);
+    WeatherAnomaly toEntity(WeatherAnomalyDTO dto);
 }
