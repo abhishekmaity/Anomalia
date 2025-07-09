d0dc44d7c559b891691415a1807e9b49de785f25
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-07-09 22:42:21 +0530

    Create EpidemicEventDTO.java

---
+++ backend/src/main/java/com/anomalia/backend/dto/EpidemicEventDTO.java
@@ -0,0 +1,13 @@
+package com.anomalia.backend.dto;
+
+import java.time.LocalDate;
+
+public class EpidemicEventDTO {
+    private Long id;
+    private String indicator;
+    private String country;
+    private LocalDate date;
+    private Double value;
+
+    // Getters and Setters
+}
