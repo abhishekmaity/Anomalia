67607909e14e738ff6b1741c810b26f69d346597
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-07-09 22:45:25 +0530

    Update EpidemicService.java

--- backend/src/main/java/com/anomalia/backend/service/EpidemicService.java
+++ backend/src/main/java/com/anomalia/backend/service/EpidemicService.java
@@ -1,25 +1,27 @@
 package com.anomalia.backend.service;
 
+import com.anomalia.backend.dto.EpidemicEventDTO;
+import com.anomalia.backend.mapper.EpidemicMapper;
 import com.anomalia.backend.model.EpidemicEvent;
 import com.anomalia.backend.repository.EpidemicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class EpidemicService {
 
     private final EpidemicRepository repository;
+    private final EpidemicMapper mapper;
 
-    public EpidemicService(EpidemicRepository repository) {
+    @Autowired
+    public EpidemicService(EpidemicRepository repository, EpidemicMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
-    public List<EpidemicEvent> getAllRecent() {
-        return repository.findAll();
-    }
-
-    public void save(EpidemicEvent event) {
-        repository.save(event);
+    public Page<EpidemicEventDTO> getRecentEvents(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("date").descending());
+        return repository.findAll(pageable).map(mapper::toDTO);
     }
 }
