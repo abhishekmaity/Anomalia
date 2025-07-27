fe299051ccccb30ebf3f59162f50d88c6fafce47
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-07-27 23:10:28 +0530

    Create NotFound.jsx

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+// src/pages/NotFound.jsx
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>404</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
